7f85e71b2dde947cc7fad09a1c4ea73b56e00b67
wireql <dmtr.off.p@gmail.com>
2024-12-26 09:33:47 +0300

    feat: add auth system

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register'])->name('user.register');
+Route::post('/login', [AuthController::class, 'login'])->name('user.login');
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('user.logout'); 
+    
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
